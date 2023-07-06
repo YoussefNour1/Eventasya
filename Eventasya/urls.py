7a486721270ef7059426c9c8a4079275b0ec036a
Youssef Nour <81862730+YoussefNour1@users.noreply.github.com>
2023-07-06 18:28:39 +0300

    initial

--- Eventasya/urls.py
+++ Eventasya/urls.py
@@ -35,3 +35,4 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('posts/', include('posts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
